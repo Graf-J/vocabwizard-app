d4921e1d1f715b52d6347f58e8fe5ec2486f6616
Graf-J <johannes.graf.1908@gmail.com>
2023-12-28 20:29:30 +0100

    Add basic Internet Connection Check

--- app/src/main/java/com/graf/vocab_wizard_app/ui/MainActivity.kt
+++ app/src/main/java/com/graf/vocab_wizard_app/ui/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.graf.vocab_wizard_app.ui
 
 import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.widget.Toast
 import com.graf.vocab_wizard_app.R
 
 class MainActivity : AppCompatActivity() {
@@ -20,5 +23,32 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Check if the device is connected to the internet
+        if (!isConnectedToInternet()) {
+            Toast.makeText(this, "Not connected to the internet", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun isConnectedToInternet(): Boolean {
+        val connectivityManager =
+            getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager?
+
+        if (connectivityManager != null) {
+            return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                val network = connectivityManager.activeNetwork
+                val capabilities = connectivityManager.getNetworkCapabilities(network)
+                capabilities != null &&
+                        (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+                                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR))
+            } else {
+                // For devices below Android M
+                @Suppress("DEPRECATION")
+                val networkInfo = connectivityManager.activeNetworkInfo
+                networkInfo != null && networkInfo.isConnected
+            }
+        }
+
+        return false
     }
 }
